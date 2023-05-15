27cf98980ace01ea323f40738d4ed26384defd78
zjason25 <zhijianzheng20@gmail.com>
2023-05-14 23:12:29 -0700

    parser optimized

--- cs122b-project2/src/SchoolParser.java
+++ cs122b-project2/src/SchoolParser.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class SchoolParser {
 
@@ -17,23 +18,46 @@ public class SchoolParser {
     Document dom;
     Integer fileParsed = 0;
     Integer recordInserted = 0;
-    ArrayList<String> insertStatements = new ArrayList<String>();
-    String query_string = "INSERT IGNORE school(id,name,rating,numVotes,net_cost,description,upper_SAT,lower_SAT,link_to_website,telephone,address,link_to_image) VALUES\n";
-    String genres_in_schools = " INSERT IGNORE genres_in_schools (genre_id, school_id) VALUES\n";
-    String genre = "INSERT IGNORE genre(fullname)\n";
-    String celebrity = "INSERT IGNORE celebrity(name,net_worth,industry) VALUES\n";
-    String celebrity_in_school = "INSERT IGNORE celebrities_in_schools(celebrity_id,school_id) VALUES ((SELECT MAX(id) FROM celebrity)\n";
+//    ArrayList<String> insertStatements = new ArrayList<String>();
+//    String query_string = "INSERT IGNORE school(id,name,rating,numVotes,net_cost,description,upper_SAT,lower_SAT,link_to_website,telephone,address,link_to_image) VALUES\n";
+//    String genres_in_schools = " INSERT IGNORE genres_in_schools (genre_id, school_id) VALUES\n";
+//    String genre = "INSERT IGNORE genre(fullname)\n";
+//    String celebrity = "INSERT IGNORE celebrity(name,net_worth,industry) VALUES\n";
+//    String celebrity_in_school = "INSERT IGNORE celebrities_in_schools(celebrity_id,school_id) VALUES ((SELECT MAX(id) FROM celebrity)\n";
+    HashMap<String, Integer> map = new HashMap<String, Integer>();
+    String query_school = "INSERT IGNORE school(id,name,rating,numVotes,net_cost,description,upper_SAT,lower_SAT,link_to_website,telephone,address,link_to_image) VALUES ";
+    String query_genre = "INSERT IGNORE genre(fullname) values ";
+    String query_genres_in_schools = "INSERT IGNORE genres_in_schools (genre_id, school_id) values ";
+    String query_celebrity = "INSERT IGNORE celebrity(name,net_worth,industry) values ";
+    String query_celebrities_in_schools = "INSERT IGNORE celebrities_in_schools(celebrity_id,school_id) VALUES ";
+    int celebrity_counter = 1;
+    int genre_id_counter = 0;
 
     public void runExample() {
 
         parseXmlFile();
         parseDocument();
 
-        query_string += ";";
-        System.out.println(query_string);
+        query_genre = query_genre.substring(0, query_genre.length() - 1);
+        query_school = query_school.substring(0, query_school.length() - 1);
+        query_genres_in_schools = query_genres_in_schools.substring(0, query_genres_in_schools.length() - 1);
+        query_celebrity = query_celebrity.substring(0, query_celebrity.length() - 1);
+        query_celebrities_in_schools = query_celebrities_in_schools.substring(0, query_celebrities_in_schools.length() - 1);
+
+
+        query_school += ";";
+        query_genre += ";";
+        query_genres_in_schools += ";";
+        query_celebrity += ";";
+        query_celebrities_in_schools += ";";
+
         System.out.println("files parsed: " + fileParsed);
 
-        executeQuery(insertStatements);
+        executeQuery(query_school);
+        executeQuery(query_genre);
+        executeQuery(query_genres_in_schools);
+        executeQuery(query_celebrity);
+        executeQuery(query_celebrities_in_schools);
 
     }
     private void parseXmlFile() {
@@ -52,6 +76,7 @@ public class SchoolParser {
         }
     }
 
+
     private void parseDocument() {
         // get the document root Element
         Element documentElement = dom.getDocumentElement();
@@ -68,20 +93,33 @@ public class SchoolParser {
             updateQuery(school);
             // add it to list
             schools.add(school);
-            insertLocationIntoDatabase(school);
+//            insertLocationIntoDatabase(school);
         }
     }
 
     private void updateQuery(school school) {
-        query_string += "\"" + school.get_school_id() + "\", \"" + school.get_school_name() +"\", " + school.get_rating() + ", "  + school.get_numVotes() + ", "+ school.get_net_cost() +", \""+
-                school.get_description() + "\", " + school.get_upper_SAT() +", " + school.get_lower_SAT() +", \"" +school.get_link_to_website() + "\", '" + school.get_telephone() + "', '" +
-                school.get_address()+ "', \"" + school.get_link_to_image()+ "\")";
-        if (fileParsed < 12000) {
-            query_string += ",";
+        query_school += " ('" + school.get_school_id() + "', '" + school.get_school_name() +"', " + school.get_rating() + ", "  + school.get_numVotes() + ", "+ school.get_net_cost() +", '"+
+                school.get_description() + "', " + school.get_upper_SAT() +", " + school.get_lower_SAT() +", '" +school.get_link_to_website() + "', '" + school.get_telephone() + "', '" +
+                school.get_address()+ "', '" + school.get_link_to_image()+"'),\n";
+
+        ArrayList<String> temp = new ArrayList<String>();
+        if (map.containsKey(school.get_genre())) {
+            query_genres_in_schools += String.format(" (%s, '%s'),",map.get(school.get_genre()),school.get_school_id() );
+        } else {
+            map.put(school.get_genre(), genre_id_counter);
+            query_genre += String.format("('%s'),", school.get_genre());
+            query_genres_in_schools += String.format(" (%s, '%s'),",Integer.toString(genre_id_counter),school.get_school_id() );
+            genre_id_counter++;
         }
+        List<celebrity> celebrities = school.get_celebrities();
+        for(celebrity celebrity:celebrities){
+            query_celebrity += String.format(" ('%s',%s,'%s'),",celebrity.get_name(), celebrity.get_net_worth(),celebrity.get_industry() );
+            query_celebrities_in_schools += String.format(" (%s, '%s'),", Integer.toString(celebrity_counter), school.get_school_id() );;
+            celebrity_counter ++;
+        };
     }
 
-    private void executeQuery(ArrayList<String> query_list) {
+    private void executeQuery(String query) {
         String loginUser = "mytestuser";
         String loginPasswd = "My6$Password";
         String loginUrl = "jdbc:mysql://localhost:3306/collegedb";
@@ -93,17 +131,14 @@ public class SchoolParser {
                 // create a connection to the database
                 Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
                 connection.setAutoCommit(false);
-                Statement statement = connection.createStatement();
-                for (String query: query_list) {
-                    statement.executeUpdate(query);
-                    recordInserted++;
-                }
-                connection.commit();
-                System.out.println(recordInserted + " records inserted");
+                PreparedStatement statement = connection.prepareStatement(query);
+                System.out.println(query);
+                statement.executeUpdate(query);
+                int rowsAffected = statement.executeUpdate();
+                System.out.println("Rows inserted: " + rowsAffected);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -138,11 +173,11 @@ public class SchoolParser {
                         id = rs2.getInt("id");
                     }
                     String query3 = String.format(" INSERT IGNORE genres_in_schools (genre_id, school_id) VALUES (%s, '%s');",id,school.get_school_id() );
-                    insertStatements.add(query3);
+//                    insertStatements.add(query3);
                 }
                 else{
                     String query2 = String.format("INSERT IGNORE genre(fullname) values (\"%s\");",school.get_genre());
-                    insertStatements.add(query2);
+//                    insertStatements.add(query2);
 
                     String query3 = "SELECT id FROM genre WHERE fullname = \""+school.get_genre()+"\";";
                     Statement statement3 = connection.createStatement();
@@ -152,17 +187,17 @@ public class SchoolParser {
                         id = rs3.getInt("id");
                     }
                     String query4 = String.format(" INSERT IGNORE genres_in_schools (genre_id, school_id) VALUES (%s, '%s');",id,school.get_school_id() );
-                    insertStatements.add(query4);
+//                    insertStatements.add(query4);
                 }
 
                 List<celebrity> celebrities = school.get_celebrities();
                 for(celebrity celebrity:celebrities){
 
                     String query5 = String.format("INSERT IGNORE celebrity(name,net_worth,industry) VALUES ('%s',%s,'%s');", celebrity.get_name(), celebrity.get_net_worth(),celebrity.get_industry() );;
-                    insertStatements.add(query5);
+//                    insertStatements.add(query5);
 
                     String query6= String.format("INSERT IGNORE celebrities_in_schools(celebrity_id,school_id) VALUES ((SELECT MAX(id) FROM celebrity), '%s');",school.get_school_id() );;
-                    insertStatements.add(query6);
+//                    insertStatements.add(query6);
 
                 }
                 connection.close();
